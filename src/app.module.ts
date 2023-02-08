c7720a2d077031be4a7c3335d91c6234b563c576
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2023-02-08 11:30:25 -0500

    feat: First query graphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloWorldModule,
+  ],
   controllers: [],
   providers: [],
 })
