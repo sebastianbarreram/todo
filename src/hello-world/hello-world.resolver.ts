cccb40791a2de5935b94aad5a8445cb7c28fc945
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2023-02-08 11:57:42 -0500

    feat: Queries name and getRandomNumber query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { name: 'saludo', description: 'Retorna Hola Mundo' })
   helloWorld(): string {
     return 'Hola Mundo';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
