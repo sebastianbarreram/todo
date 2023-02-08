3c305a351a58fd4d88fba411b4d1acbd865d9b65
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2023-02-08 11:47:49 -0500

    feat: Apollo server install

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
     }),
     HelloWorldModule,
   ],
